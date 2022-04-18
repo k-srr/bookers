f819d72bdc28d759937f70fb701d3af1b39cc08b
k-srr <sa.12kk28@gmail.com>
2022-04-18 16:19:02 +0000

    [add]edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/index'
+    redirect_to '/books'
   end
 
   def edit
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
   end
 
   private
