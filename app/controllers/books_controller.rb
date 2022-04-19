2e2fc83d64fc0d8b2de8e1e60f2cbd08abf8b4d9
k-srr <sa.12kk28@gmail.com>
2022-04-19 16:25:18 +0000

    [add]edit show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,11 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
 
@@ -32,7 +35,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(@book.id)
+    redirect_to book_path(book.id)
   end
 
   private
