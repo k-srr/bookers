c6969b86e2117bd1e0796ecb865f097c673b42a0
k-srr <sa.12kk28@gmail.com>
2022-04-22 13:18:34 +0000

    [add]errorcheck

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,11 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
     else
       render :index
     end
@@ -15,6 +14,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
 
   end
 
@@ -33,14 +33,18 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 
